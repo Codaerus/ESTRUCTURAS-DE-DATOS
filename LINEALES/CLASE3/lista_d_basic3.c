d27ca0f35e7ad5414a48c81b1e0b963f209bc9f4
Codaerus <christianqc91@gmail.com>
2024-12-28 22:44:53 -0500

    Create lista_d_basic3.c

---
+++ LINEALES/CLASE3/lista_d_basic3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Nodo {
+    int dato;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+};
+
+void insertarFinal(struct Nodo** cabeza, int dato){
+    struct Nodo* nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->siguiente = NULL;
+    nuevoNodo->anterior = NULL;
+    
+    if(*cabeza == NULL)
+        {
+            *cabeza = nuevoNodo;
+        }
+    else{
+        struct Nodo* temp = *cabeza;
+        while(temp->siguiente != NULL){
+            temp = temp->siguiente;
+        }
+        temp->siguiente = nuevoNodo;
+        nuevoNodo->anterior = temp;
+    }
+}
+
+void insertarPrincipio(struct Nodo** cabeza, int dato){
+    struct Nodo* nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->siguiente = *cabeza;
+    nuevoNodo->anterior = NULL;
+    if(*cabeza != NULL){
+        (*cabeza)->anterior = nuevoNodo;
+    }
+    *cabeza = nuevoNodo;
+}
+
+int main() {
+    struct Nodo* cabeza = NULL;
+    insertarFinal(&cabeza,10);
+    insertarFinal(&cabeza,20);
+    insertarFinal(&cabeza,30);
+    
+    insertarPrincipio(&cabeza,5);
+    insertarPrincipio(&cabeza,1);
+    insertarPrincipio(&cabeza,7);
+    //Recorre hacia adelante
+    struct Nodo* actual = cabeza;
+    while(actual != NULL){
+        printf("%d -> ", actual->dato);
+        actual = actual->siguiente;
+    }
+    printf("NULL\n");
+    //Liberar memoria
+    while(cabeza != NULL){
+        struct Nodo* temp = cabeza;
+        cabeza = cabeza->siguiente;
+        free(temp);
+    }
+    return 0;
+}
